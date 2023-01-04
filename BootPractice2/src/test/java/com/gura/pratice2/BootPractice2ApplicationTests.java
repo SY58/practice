5e89506e2bb5ebc66be8939b9732923fd847308e
CheonOkHui <cjsdhrgml000@gmail.com>
2023-01-04 17:06:24 +0900

    practice

---
+++ BootPractice2/src/test/java/com/gura/pratice2/BootPractice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gura.pratice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootPractice2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
